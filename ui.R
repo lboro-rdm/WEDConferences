cee50df55b6a41c929dfdf85d39ed713ff7a1e2c
lboro-rdm <rdm@lboro.ac.uk>
2025-04-07 15:40:02 +0100

    new css

--- ui.R
+++ ui.R
@@ -9,45 +9,71 @@ library(dplyr)
 ui <- tags$html(
   lang = "en",
   fluidPage(
-    titlePanel(
-      HTML('<span style="color: #002c3d;"><strong>WEDC, Loughborough University:</strong></span>
-          <span style="color: #009BC9;">Conferences</span><br><br>')
-    ),
+    style = "padding: 0px; margin: 0px;",
     tags$head(
-      tags$style(HTML("
-        body {
-          background-color: #FFFFFF;
-          font-size: 16px;
-        }
-        h2, a {
-          color: #6F3092;
-        }
-        a.hover-underline:hover {
-          text-decoration: underline;
-        }
-      "))
+      tags$title("WEDC: Conferences"),
+      tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
     ),
+    
+    # Black banner
+    tags$div(
+      class = "black-banner",
+      tags$div(
+        class = "banner-content",
+        tags$a(
+          href = "https://www.lboro.ac.uk",
+          target = "_blank",
+          tags$img(src = "logo.png", class = "uni-logo", alt = "University Logo")
+        ),
+        tags$span("School of Architecture, Building and Civil Engineering")
+      )
+    ),
+    
+    # Blue banner
+    tags$div(
+      class = "blue-banner",
+      tags$div(
+        class = "banner-content",
+        tags$span("Water Engineering and Development Centre"),
+        tags$a(
+          href = "https://www.lboro.ac.uk/research/wedc/publications-and-resources/",
+          class = "return-link",
+          "< Return to Publications and resources"
+        )
+      )
+    ),
+    
+    # Title section
+    tags$div(
+      class = "white-banner",
+      tags$h1("Conferences")
+    ),
+    
     sidebarLayout(
       sidebarPanel(
-        style = "margin-top: 20px;",
+        style = "margin-left: 20px; padding-right: 20px;",
         selectInput(
           inputId = "collectionSelect",
-          label = "Select a Conference:",
+          label = "Select a Conference",
           choices = NULL,
           selected = NULL,
           multiple = FALSE
         ),
         textInput(
           inputId = "authorSearch",
-          label = "Search by Author:",
+          label = "Search by Author",
           placeholder = "Enter author's name"
         ),
         textInput(
           inputId = "titleSearch",
-          label = "Search by Title:",
+          label = "Search by Title",
           placeholder = "Enter book or manual title"
-        )
+        ),
+        p(),
+        p("The Water Engineering and Development Centre (WEDC) produces and disseminates quality, relevant and accessible knowledge products to meet the needs of academics, policymakers and practitioners working in various aspects of water engineering and development."),
+        p("Our books, manuals and other resources represent a substantial body of knowledge in water management, engineering and other international development-related subjects developed over 50 years.")
       ),
+      
       mainPanel(
         fluidRow(
           style = "margin-left: 20px; margin-right: 20px;",
@@ -56,6 +82,20 @@ ui <- tags$html(
             type = 3,
             color = "#009BC9",
             color.background = "#FFFFFF"
+          )
+        )
+      )
+    ),
+    
+    # Footer OUTSIDE of sidebarLayout
+    tags$div(
+      class = "footer",
+      fluidRow(
+        column(
+          12,
+          tags$a(
+            href = 'https://doi.org/10.17028/rd.lboro.28525481',
+            "Accessibility Statement"
           )
         )
       )
